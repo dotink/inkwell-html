80697ba1c1588a995b6c60af47690a94c5464946
Matthew J. Sahagian <msahagian@dotink.org>
2015-07-03 01:27:12 -0700

    Comment added

--- src/Commands/lower.php
+++ src/Commands/lower.php
@@ -2,6 +2,13 @@
 {
 	class lower
 	{
+		/**
+		 * Make a value lowercase
+		 *
+		 * @access public
+		 * @param mixed The value to make lowercase
+		 * @return mixed The lowercased value, or original value if not a string
+		 */
 		public function __invoke($data)
 		{
 			return is_string($data)
