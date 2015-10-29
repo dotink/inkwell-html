7f435e9ac861a433308c1e258c55e79a128c61b4
Matthew J. Sahagian <msahagian@dotink.org>
2015-10-28 22:28:45 -0700

    Don't settype if traversable

--- src/Commands/per.php
+++ src/Commands/per.php
@@ -1,12 +1,16 @@
 <?php namespace Inkwell\HTML
 {
+	use Traversable;
+
 	class per
 	{
 		public function __invoke($data, $callback)
 		{
 			$data = $data ?: array();
 
-			settype($data, 'array');
+			if (!is_object($data) || !$data instanceof Traversable) {
+				settype($data, 'array');
+			}
 
 			foreach ($data as $i => $value) {
 				$callback(html::out($i), html::out($value));
